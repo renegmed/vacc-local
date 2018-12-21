b6be5dc02e69354dab1b12cbbdf695d97a2151a7
rene <renegmed@yahoo.com>
2018-12-20 23:02:16 -0500

    start of frontend dev

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import logo from './img/Vac-Acc-logo.png';
-import './App.css';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Button bsStyle='danger'>
-            Insert Uport
-          </Button>
-        </header>
+ 
+      <div>
+         <Route exact path="/" render={ () => (
+            <div>
+              Main Page
+            </div>    
+         )} />
+
+         <Route exact path="/Attestation" render={ () => (
+            <div>
+              Attestation Page
+            </div>    
+         )} />
+ 
       </div>
     );
   }
